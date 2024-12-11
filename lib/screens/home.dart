8f354212569c5226d41383964f69be940e9d8661
Tedysek01 <tadeas@raska.eu>
2024-12-11 23:38:52 +0100

    11.12.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,7 +9,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 const CameraPosition _kGooglePlex = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
+  target: LatLng(50.073658, 14.418540),
   zoom: 14.4746,
 );
 
